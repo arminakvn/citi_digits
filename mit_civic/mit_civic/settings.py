6fbd84b030c8611db44c7c304fa92f5ab497a544
sw2279 <sew.williams@gmail.com>
2013-09-23 20:20:44 -0400

    removed sensitive info

--- mit_civic/mit_civic/settings.py
+++ mit_civic/mit_civic/settings.py
@@ -20,8 +20,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'citi_digits',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'city_user',
-        'PASSWORD': 'Eo@_rKR7)ZohFWT',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }

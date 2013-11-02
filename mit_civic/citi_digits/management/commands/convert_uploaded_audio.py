5437a0a00868280252c47e1d86ac760a624d23be
root <root@ghostmap.mit.edu>
2013-11-02 17:59:26 -0400

    added conversion script

--- mit_civic/citi_digits/management/commands/convert_uploaded_audio.py
+++ mit_civic/citi_digits/management/commands/convert_uploaded_audio.py
@@ -12,34 +12,32 @@ class Command(BaseCommand):
     args = ''
     help = ''
 
-    def convertAMRToMp3(self,dirpath,filename):
-        pathToAMR = os.join(dirpath,filename)
-        pathToMP3 = os.join(dirpath,"%s.mp3" % (filename))
+    def convertAMRToMp3(self,(dirpath,filename)):
+        pathToAMR = os.path.join(dirpath,filename)
+        pathToMP3 = os.path.join(dirpath,"%s.mp3" % (filename))
 
         print "Creating file: " + pathToMP3
-
+          
         # ffmpeg -i .amr -acodec libmp3lame -ab 64k test.mp3
         call(["ffmpeg", "-i",pathToAMR,"-acodec","libmp3lame","-ab","64k",pathToMP3])
 
-    def findAMRFiles(self, paths):
-        for dirpath, dirnames, filenames in paths:
-            fileToProcess = None
-            alreadyEncodedFlag = False
-            for filename in filenames:
-                if ".amr" in filename:
-                    fileToProcess = (dirpath,filename)
-                if ".mp3" in filename:
-                    alreadyEncodedFlag = True
-            if (alreadyEncodedFlag):
-                print "Working on file: " + fileToProcess[1]
-                self.convertAMRToMp3(fileToProcess)
+    def findAMRFiles(self, dirpath,dirnames,filenames):
+        fileToProcess = None
+        alreadyEncodedFlag = False
+        for filename in filenames:
+            if ".amr" in filename:
+                fileToProcess = (dirpath,filename)
+            if ".mp3" in filename:
+                alreadyEncodedFlag = True
+        if (not alreadyEncodedFlag and fileToProcess is not None):
+             self.convertAMRToMp3(fileToProcess)
 
     def getDirectoryListing(self):
         """
             Returns a listing of the application's audio directory
         """
-        for dirpath, dirnames, filenames in os.walk('../../../media/audio/'):
-            self.findAMRFiles((dirpath, dirnames, filenames))
+        for dirpath, dirnames, filenames in os.walk(os.path.dirname(os.path.realpath(__file__)) + "/../../../media/audio/"):
+            self.findAMRFiles(dirpath, dirnames, filenames)
 
     def handle(self, *args, **options):
         print "Starting audio conversions...."
